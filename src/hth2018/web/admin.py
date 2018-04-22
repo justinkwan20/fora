38fae726f9f3649fec97259b2a36d2245e773ff2
wpine215 <wpine215@gmail.com>
2018-04-22 03:16:44 +0000

    Updated frontend

--- src/hth2018/web/admin.py
+++ src/hth2018/web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+from .models import Listing
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username','displayname','password','email']
+
+class ListingAdmin(admin.ModelAdmin):
+    fields = ['title','desc','price','ratingJSON']
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(User, UserAdmin)
--- src/hth2018/web/views.py
+++ src/hth2018/web/views.py
@@ -6,12 +6,29 @@ from web.models import Listing
 
 # Create your views here.
 def index(request):
-    user = User.objects.create(username = "wpine215", displayname = "Will", password = "password", email = "wpine@bu.edu")
-    listing = Listing.objects.create(title = "Test listing", desc = "This is a test listing.", price = 25, ratingJSON = "5")
-    user.save()
-    listing.save()
-    template = loader.get_template("web/index.html")
-    return HttpResponse(template.render({"user_id": user.id, "username": user.username, "title": listing.title, "price": listing.price, "rating": listing.ratingJSON}, request))
+    # This creates temporary database entries
+    # user = User.objects.create(username = "wpine215", displayname = "Will", password = "password", email = "wpine@bu.edu")
+    # listing = Listing.objects.create(title = "Test listing", desc = "This is a test listing.", price = 25, ratingJSON = "5")
+    # user.save()
+    # listing.save()
 
+    # This loads the template and returns it to the user
+    latest_listings = Listing.objects.order_by('-id')[:10]
+    context = {'latest_listings': latest_listings}
+    return render(request, 'web/index.html', context)
+    #template = loader.get_template("web/index.html")
+    #context = {
+    #    "user_id": user.id,
+    #    "username": user.username,
+    #    "title": listing.title,
+    #    "price": listing.price,
+    #    "rating": listing.ratingJSON
+    #    }
+    #return HttpResponse(template.render(context, request))
+
+def new_listing(request):
+    # database stuff
+    template = loader.get_template("web/newlisting.html")
+    pass
 # example:
 # user.objects.get(id = user.id)
